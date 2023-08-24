e5aabb3cd2ba68b214ba08c4e34768bea32eb30a
Ajan Eshwara <ajan.eshwara@gmail.com>
2023-08-24 23:09:27 +0530

    Third commit

--- BankingApp_Final.java
+++ BankingApp_Final.java
@@ -14,6 +14,7 @@ public class BankingApp_Final {
         final String DASHBOARD   = "Welcome to Smart Banking App";
         final String NEW_ACCOUNT = "Open New Account";
         final String DEPOSITS    = "Deposits";
+        final String WITHDRAWLS  = "Withdrawls";
         final String TRANSFERS   = "Transfers";
         final String DELETE_ACC  = "Delete Account";
         final String PRINT_ACC   = "Print Account";
@@ -34,6 +35,49 @@ public class BankingApp_Final {
         System.out.println("-".repeat(45));
         System.out.println("\t" + APP_TITLE );
         System.out.println("-".repeat(45));
+
+        do{
+            switch(screen){
+                case DASHBOARD: 
+                System.out.println("\t[1]. Open New Account");
+                System.out.println("\t[2]. Depositst");
+                System.out.println("\t[3]. Withdrawls");
+                System.out.println("\t[4]. Transfers");
+                System.out.println("\t[5]. Delete Account");
+                System.out.println("\t[6]. Print Account");
+                System.out.println("\t[7]. Exit\n");
+                System.out.println("-".repeat(45));
+                System.out.print("\tEnter an option to continue: ");
+                int option = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (option){
+                    case 1: screen = NEW_ACCOUNT; break;
+                    case 2: screen = DEPOSITS; break;
+                    case 3: screen = WITHDRAWLS; break;
+                    case 4: screen = TRANSFERS; break;
+                    case 5: screen = DELETE_ACC; break;
+                    case 6: screen = PRINT_ACC; break;
+                    case 7: System.out.println(CLEAR); System.exit(0);
+                    default: continue;
+                }
+                break;
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+        }while(true);
         
     }
     
